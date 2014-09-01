807219199c648ed88667c85ee0cd9787364bcc8c
Robert Deniszczyc <robdeniszczyc@gmail.com>
2014-09-01 20:21:12 +0100

    title style if no splash image

--- front-page.php
+++ front-page.php
@@ -6,6 +6,10 @@ if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts
 
         <article role="main" class="primary-content type-post" id="post-<?php the_ID(); ?>">
             <div class="guest-content">
+            <?php
+            $splash_image = get_field('splash_image');
+            if ($splash_image!='') {
+            ?>
                 <div class="splash">
                    <div class="splash__title__container">
                         <div class="splash__title">
@@ -17,7 +21,17 @@ if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts
                         </div>
                     </div>
                     <img class="splash__image" src="<?php the_field('splash_image'); ?>" alt=""/>
-                </div>  
+                </div>
+                <?php } else { ?>
+                    <div class="article__title">
+                        <h1><?php echo get_the_title(); ?></h1>
+                        <p>by <?php the_field('post_author'); ?> </p>
+                        <p>Published on <time datetime="<?php the_time('l, F jS, Y') ?>" pubdate><?php the_time('l, F jS, Y') ?></time></p>
+                        <hr>
+                    </div>
+                <?php
+                    }  
+                ?>
                 <div class="article__content">
                     <?php the_content(); ?>
                 </div>
