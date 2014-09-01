351d72b0c4d8bbe0b1d63eda166981801aab5358
Robert Deniszczyc <robdeniszczyc@gmail.com>
2014-09-01 20:24:44 +0100

    title styling single.php

--- single.php
+++ single.php
@@ -4,8 +4,12 @@
 
         <article role="main" class="primary-content type-post" id="post-<?php the_ID(); ?>">
             <div class="guest-content">
+                <?php
+            $splash_image = get_field('splash_image');
+            if ($splash_image!='') {
+            ?>
                 <div class="splash">
-                    <div class="splash__title__container">
+                   <div class="splash__title__container">
                         <div class="splash__title">
                             <h1><?php echo get_the_title(); ?></h1>
                             <p>by <?php the_field('post_author'); ?> </p>
@@ -15,7 +19,17 @@
                         </div>
                     </div>
                     <img class="splash__image" src="<?php the_field('splash_image'); ?>" alt=""/>
-                </div>  
+                </div>
+                <?php } else { ?>
+                    <div class="article__title">
+                        <h1><?php echo get_the_title(); ?></h1>
+                        <p>by <?php the_field('post_author'); ?> </p>
+                        <p>Published on <time datetime="<?php the_time('l, F jS, Y') ?>" pubdate><?php the_time('l, F jS, Y') ?></time></p>
+                        <hr>
+                    </div>
+                <?php
+                    }  
+                ?>
                 <div class="article__content">
                     <?php the_content(); ?>
                     <br />
