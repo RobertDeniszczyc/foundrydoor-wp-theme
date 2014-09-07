b068719b8ecfe7ee48f1ab926e1337578290c9d3
Robert Deniszczyc <robdeniszczyc@gmail.com>
2014-09-07 19:27:52 +0100

    hide password protected posts

--- functions.php
+++ functions.php
@@ -97,4 +97,20 @@ if (function_exists('register_nav_menu'))
     register_nav_menu('nav__secondary', 'Secondary Nav');
 }
 
+// Filter to hide protected posts
+function exclude_protected($where) {
+	global $wpdb;
+	return $where .= " AND {$wpdb->posts}.post_password = '' ";
+}
+
+// Decide where to display them
+function exclude_protected_action($query) {
+	if( !is_single() && !is_page() && !is_admin() ) {
+		add_filter( 'posts_where', 'exclude_protected' );
+	}
+}
+
+// Action to queue the filter at the right time
+add_action('pre_get_posts', 'exclude_protected_action');
+
 ?>
